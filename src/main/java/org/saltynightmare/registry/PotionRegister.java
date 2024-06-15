7edfcbd32494af43a8f15d8a6209bbe645d74963
Artemiy Schukin <shuckinart@yandex.ru>
2024-06-15 21:14:24 +0300

    change potion registering a little

--- src/main/java/org/saltynightmare/registry/PotionRegister.java
+++ src/main/java/org/saltynightmare/registry/PotionRegister.java
@@ -2,7 +2,6 @@ package org.saltynightmare.registry;
 
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
 import net.minecraft.registry.Registries;
@@ -11,19 +10,19 @@ import net.minecraft.registry.entry.RegistryEntry;
 import org.saltynightmare.SaltyNightmare;
 
 public class PotionRegister {
-    public static final RegistryEntry<Potion> SALTY_POTION = registerPotion("salty_potion", new StatusEffectInstance[]{ new StatusEffectInstance(EffectsRegister.THIRST, 100, 0) });
+    public static final RegistryEntry<Potion> SALTY_POTION;
 
     public static void registerPotions() {
-        registerRecipe(Potions.WATER, ItemsRegister.SALT_POWDER, SALTY_POTION);
-    }
-
-    protected static void registerRecipe(RegistryEntry<Potion> input, Item item, RegistryEntry<Potion> output) {
         FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.registerPotionRecipe(input, item, output);
+            builder.registerPotionRecipe(Potions.WATER, ItemsRegister.SALT_POWDER, SALTY_POTION);
         });
     }
 
     protected static RegistryEntry<Potion> registerPotion(String name, StatusEffectInstance[] effect) {
         return Registry.registerReference(Registries.POTION, SaltyNightmare.id(name), new Potion(effect));
+    }
+
+    static {
+        SALTY_POTION = registerPotion("salty_potion", new StatusEffectInstance[]{ new StatusEffectInstance(EffectsRegister.THIRST, 100, 0) });
     }
 }
